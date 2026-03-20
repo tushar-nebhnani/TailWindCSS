53cc6545320134eff0280cd580bd1903e24c3acb
Tushar <nebhnani.tushar8@gmail.com>
2026-03-21 00:29:52 +0530

    Validation done

---
+++ module/engine/input.js
@@ -0,0 +1,34 @@
+import { showError } from "./module/ui/reporter.js";
+
+export function takeHTMLInput() {
+  const inputHTML = document.getElementById("html-input");
+  if (!inputHTML) {
+    showError("Terminal Error: html-input element not found in DOM.");
+    return null;
+  }
+  const htmlContent = inputHTML.value.trim();
+
+  if (!htmlContent) {
+    showError("Error 404: No HTML content found.");
+    return null;
+  }
+
+  return htmlContent;
+}
+
+export function takeCSSInput() {
+  const inputCSS = document.getElementById("input-css");
+  if (!inputCSS) {
+    showError("Terminal Error: css-input element not found in DOM.");
+    return null;
+  }
+
+  const cssContent = inputCSS.value.trim();
+
+  if (!cssContent) {
+    showError("Error 404: No CSS content found.");
+    return null;
+  }
+
+  return cssContent;
+}
---
+++ module/engine/validator.js
@@ -0,0 +1,42 @@
+import { showError } from "./module/ui/reporter.js";
+
+function isSafe(str) {
+  const lowerStr = str.toLowerCase();
+  if (lowerStr.includes("<script") || lowerStr.includes("onclick")) {
+    showError("SECURITY BREACH: Scripting tokens are forbidden in the buffer.");
+    return false;
+  }
+  return true;
+}
+
+export function validHTML(htmlContent) {
+  const livePreviewBody = document.getElementById("live-body");
+  const htmlPattern = /<\/?[a-z][\s\S]*>/i;
+
+  // Check 1: Empty check
+  if (!htmlContent) {
+    showError("BUFFER EMPTY: Please enter HTML content.");
+    return false;
+  }
+
+  // Check 2: Pattern Match
+  if (!htmlPattern.test(htmlContent)) {
+    showError("SYNTAX ERROR: No valid HTML tags detected.");
+    return false;
+  }
+
+  // Check 3: Security
+  if (!isSafe(htmlContent)) return false;
+
+  // Check 4: Structural Integrity (The Virtual Test)
+  const virtualDiv = document.createElement("div");
+  virtualDiv.innerHTML = htmlContent;
+
+  if (virtualDiv.children.length === 0) {
+    showError("DOM ERROR: Input contains text but no renderable elements.");
+    return false;
+  }
+
+  livePreviewBody.innerHTML = htmlContent;
+  return true;
+}
